99dc95e2ba2d630ab3cb2b411a6ce3dc07b31ebb
John Pierce <pierce.johndaniel@gmail.com>
2021-02-11 17:47:46 -0600

    feat:added loops.html with while,for and break and continue.js files

---
+++ js/for_loops.js
@@ -0,0 +1,97 @@
+
+"use strict";
+/*
+Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+For example, showMultiplicationTable(7) should output
+7 x 1 = 7
+7 x 2 = 14
+7 x 3 = 21
+7 x 4 = 28
+7 x 5 = 35
+7 x 6 = 42
+7 x 7 = 49
+7 x 8 = 56
+7 x 9 = 63
+7 x 10 = 70
+ */
+
+// function showMultiplicationTable(num){
+//
+//     for (var i = 1; i <= 10; i++) {
+//             console.log(num + " x " + i + " = " + num * i);
+//
+//     }
+// }
+// showMultiplicationTable(7)
+
+/*
+Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+123 is odd
+80 is even
+24 is even
+199 is odd
+...
+ */
+
+// for(var i = 1; i <= 10; i++){
+//     var randomNum = Math.floor(Math.random() * 201);
+//     if(randomNum % 2 === 0){
+//         console.log(randomNum + " is even");
+//     } else if(randomNum % 2 !== 0){
+//         console.log(randomNum + " is odd");
+//     }
+// }
+
+/*
+Create a for loop that uses console.log to create the output shown below.
+1
+22
+333
+4444
+55555
+666666
+7777777
+88888888
+999999999
+ */
+
+    var totalNumberofRows = 9;
+    var num = ""
+    for (var i = 1; i <= totalNumberofRows; i++) {
+        for (var j = 1; j <= i; j++) {
+            num = num + j + '  ';
+        }
+        console.log(num);
+
+    }
+
+
+
+
+/*
+Create a for loop that uses console.log to create the output shown below.
+100
+95
+90
+85
+80
+75
+70
+65
+60
+55
+50
+45
+40
+35
+30
+25
+20
+15
+10
+5
+ */
+
+for(i = 100; i >= 5; i--){
+    console.log(i);
+}
