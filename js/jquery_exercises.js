1313769b55da7a514ad5f14219f292102f490566
John Pierce <pierce.johndaniel@gmail.com>
2021-04-05 17:07:25 -0500

    feat: working through exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,15 @@
 "use strict"
 
+$('*').css('padding', '5px 0 5px 0');
+
+
 $(document).ready(function (){
-    alert("The DOM has finished loading.");
+    var content = $("#header").html()
+    alert(content);
+
 });
+
+
+// $(".zombie-bgc").css("background-color", "#698362");
+//
+// $(".code-up").css("border", "1px wide red")
