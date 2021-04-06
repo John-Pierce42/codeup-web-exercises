8bf89c1ee1e55da216ff17003702f14584d4eef6
John Pierce <pierce.johndaniel@gmail.com>
2021-04-06 11:44:45 -0500

    feat: working through jquery exercise and added background img

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,6 +1,6 @@
 "use strict"
 
-$('*').css('padding', '5px 0 5px 0');
+// $('*').css('padding', '5px 0 5px 0');
 
 
 $(document).ready(function (){
@@ -8,8 +8,13 @@ $(document).ready(function (){
     alert(content);
 
 });
+$("body").css("margin", "20px 100px 20px 100px");
+$("body").css({"background-image": "url( img/yohann-libot-f7NnOkM1yeU-unsplash.jpg)", "background-size" : "100% 100%", " background-repeat":"no-repeat", });
+$("#header").css("text-align", "center");
+$(".codeup").css("text-align", "center");
+$("li").css({"list-style": "none", "font-size" : "20px"});
+
+$(".codeup").css("border", "1px solid red");
+$(".zombie-bgc").css({"background-color" : "#698362","color" : "rebeccapurple"});
 
 
-// $(".zombie-bgc").css("background-color", "#698362");
-//
-// $(".code-up").css("border", "1px wide red")
