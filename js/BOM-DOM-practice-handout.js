0703b92054bd40ee66c5c8fa4ea13139b3705c7d
John Pierce <pierce.johndaniel@gmail.com>
2021-04-09 09:01:07 -0500

    feat: added practice assessment bom and dom

---
+++ js/BOM-DOM-practice-handout.js
@@ -0,0 +1,33 @@
+
+
+//TODO: When the #changeBoxSize button is clicked the box should double in size from what it was!
+
+var changeSizeBtn = document.getElementById("changeBoxSize")
+    changeSizeBtn.addEventListener("click", function (e){
+        console.log(e);
+        var targetBox = document.getElementById("box")
+        targetBox.style.height = "500px";
+        targetBox.style.width = "500px";
+
+    })
+
+//TODO: I have two CSS classes - toggleClass1 and toggleClass1 - that are currently unused. When the #toggleClass button is clicked, program a bit of JavaScript to toggle between those classes to see the background-color switch back and forth with each click. Bonus: Write your callback function in a variable and use it instead!
+
+var toggleBtn = document.getElementById("toggleClass");
+console.log(toggleBtn);
+toggleBtn.addEventListener("click", function (e){
+    var toggleBox = document.getElementById("box")
+    toggleBox.setAttribute("class", "toggleClass1");
+})
+
+//TODO: In the #boxInfo element, when the BOX is hovered over the HEIGHT and WIDTH of the box would appear in that HTML area [div]
+
+//TODO: When the user types in to our #input and then clicks #inputBtn, what they typed into the box should replace what was inside of the span #userName with the input's contents
+
+//TODO: Refactor the above work - after 5 seconds have passed from the user clicking #inputBtn and seeing #userName change, change the entire div of #helloUser to say "Let me know if you want to play a game. ."
+
+
+
+
+
+
