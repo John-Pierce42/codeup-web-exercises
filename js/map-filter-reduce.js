e16d3f3da498a8413f8cf71b2197ff51032c8e17
John Pierce <pierce.johndaniel@gmail.com>
2021-04-16 18:56:25 -0500

    feat: added map-filter-reduce.html and still working through js exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -36,4 +36,56 @@ const users = [
     }
 ];
 
-// let languages3 =
+ //***********************************************
+ let languages3 = users.filter(function (languages){
+     return languages.languages.length > 2;
+ })
+
+console.log(languages3);
+
+ //**********************************************
+ let emailArr = users.map(function (emails){
+     return emails.email
+ });
+
+console.log(emailArr);
+
+
+//**********************************************
+let yearsOfExp = users.reduce(function (total, num){
+     return  total + num.yearsOfExperience;
+
+}, 0);
+
+let averageYearsOfExp = users.reduce(function (total,num){
+    let totalYears = total + num.yearsOfExperience;
+    let averageYrs = totalYears / 5;
+    return averageYrs;
+}, 0);
+
+console.log(`Their total number years of exp are: ${yearsOfExp}!`);
+console.log(`Their total average years of exp are: ${averageYearsOfExp}!`);
+
+
+// let longestEmail = users.reduce(function (user,email){
+//         if(user.email.length > email.email.length){
+//             return user.email
+//         }
+// }, "")
+//
+// console.log(longestEmail);
+
+
+//************ stack overFlow ************
+let longestEmail = users.reduce(function (a,b){
+
+        return a.length > b.length ? a : b;
+},"");
+
+console.log(longestEmail);
+
+ // *******************************************
+  let usersNameInString =  users.reduce(function (obj,ele){
+
+
+}, "")
